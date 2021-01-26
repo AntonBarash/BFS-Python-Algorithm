53bc70ce73abc77d08905dc9ded2ce520c81eabd
AntonBarash <64368628+AntonBarash@users.noreply.github.com>
2021-01-25 19:16:55 -0500

    Create BFSAlgo.py

---
+++ BFSAlgo.py
@@ -0,0 +1,20 @@
+import sys
+from queue import *
+
+def  BFS(N, s, adj_list):
+
+    level = ['x']*N
+    discovered = [False]*N
+    Q = Queue()
+    for source in s:
+        Q.put(source)
+        level[source] = 0
+        discovered[source] = True
+    while Q.empty() == False:
+        node = Q.get()
+        for neighbor in adj_list[node]:
+            if discovered[neighbor] == False:
+                discovered[neighbor] = True
+                Q.put(neighbor)
+                level[neighbor] = level[node] + 1
+    return level
